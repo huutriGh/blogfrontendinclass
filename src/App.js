8a2fef216b9500fecd2e6e8de265f4bb96533b99
nguyen huu tri <huutriqt13@hotmail.com>
2023-04-05 21:26:07 +0700

    add pagination and delete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import {
   CardContent,
   Container,
   Grid,
+  Pagination,
   Paper,
+  Stack,
   Typography,
 } from "@mui/material";
 import "./App.css";
@@ -14,17 +16,69 @@ import { useEffect, useState } from "react";
 import client from "./api/client";
 
 function App() {
-  const [state, setState] = useState([]);
+  const [state, setState] = useState({
+    data: [],
+    first: true,
+    last: false,
+    size: 0,
+    number: 0,
+    totalElements: 0,
+    totalPages: 0,
+    empty: true,
+  });
 
-  const getBlogs = async () => {
-    const resp = await client.get("/blogs");
+  const getBlogs = async (page) => {
+    const resp = await (page
+      ? client.get(`/blogs?pageNo=${page - 1}`)
+      : client.get("/blogs"));
     console.log(resp.data);
-    setState(resp.data.content);
+
+    const {
+      empty,
+      first,
+      last,
+      numberOfElements,
+      totalElements,
+      totalPages,
+      content,
+      number,
+    } = resp.data;
+
+    setState((prev) => ({
+      ...prev,
+      empty,
+      first,
+      last,
+      numberOfElements,
+      totalElements,
+      totalPages,
+      number,
+      data: content,
+    }));
   };
+
   useEffect(() => {
     getBlogs();
   }, []);
 
+  const handleChangePage = async (event, page) => {
+    getBlogs(page);
+  };
+
+  const handleDelete = async (blog) => {
+    const resp = await client.post("/blogs/delete", blog);
+    console.log(resp);
+    if (resp.status === 200) {
+      const blogs = state.data.filter((item) => item.blogId !== blog.blogId);
+
+      setState((prev) => ({
+        ...prev,
+        data: blogs,
+      }));
+    } else {
+      alert("Delete fail");
+    }
+  };
   return (
     <Container>
       <Paper sx={{ padding: "30px" }}>
@@ -34,7 +88,7 @@ function App() {
               My blogs
             </Typography>
           </Grid>
-          {state.map((item) => (
+          {state.data.map((item) => (
             <Grid item md={4} xs={12} key={item.blogId}>
               <Card>
                 <CardContent>
@@ -47,11 +101,23 @@ function App() {
                 </CardContent>
                 <CardActions>
                   <Button>Edit</Button>
-                  <Button>DELETE</Button>
+                  <Button onClick={() => handleDelete(item)}>DELETE</Button>
                 </CardActions>
               </Card>
             </Grid>
           ))}
+
+          {!state.empty ? (
+            <Grid item md={12} xs={12}>
+              <Stack>
+                <Pagination
+                  count={state.totalPages}
+                  page={state.number + 1}
+                  onChange={handleChangePage}
+                ></Pagination>
+              </Stack>
+            </Grid>
+          ) : null}
         </Grid>
       </Paper>
     </Container>
