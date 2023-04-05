b46d9fb785cb19408a1d5decdd92cc0dbc9bfa03
nguyen huu tri <huutriqt13@hotmail.com>
2023-04-05 19:24:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
+import "./App.css";
+
+import { useEffect, useState } from "react";
+import client from "./api/client";
 
 function App() {
+  const [state, setState] = useState([]);
+
+  const getBlogs = async () => {
+    const resp = await client.get("/blogs");
+    console.log(resp.data);
+    setState(resp.data.content);
+  };
+  useEffect(() => {
+    getBlogs();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Paper sx={{ padding: "30px" }}>
+        <Grid container spacing={3}>
+          <Grid item md={12} xs={12}>
+            <Typography variant="h3" align="center">
+              My blogs
+            </Typography>
+          </Grid>
+          {state.map((item) => (
+            <Grid item md={4} xs={12} key={item.blogId}>
+              <Card>
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    {item.title}
+                  </Typography>
+                  <Typography gutterBottom variant="p">
+                    {item.url}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button>Edit</Button>
+                  <Button>DELETE</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+    </Container>
   );
 }
 
